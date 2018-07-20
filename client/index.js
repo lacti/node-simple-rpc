117428d528c9e62371f943add206b1b138f240ec
jaeyoung.choi <lactrious@gmail.com>
2018-07-20 22:28:01 +0900

    Initial commit

---
+++ client/index.js
@@ -0,0 +1,7 @@
+const proxy = require('../lib/proxy');
+const rpcProxy = proxy('http://localhost:8192')(require('./interface'));
+
+(async () => {
+  const result = await rpcProxy.add(10, 20);
+  console.log(result);
+})();
---
+++ server/index.js
@@ -0,0 +1,4 @@
+const rpc = require('../lib/rpc');
+const procedures = require('./procedures');
+
+rpc(8192, procedures);
